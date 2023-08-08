becbf5cc4a8320105dce2a5099eeeb9b87a6a57d
Aksh1551 <141571540+Aksh1551@users.noreply.github.com>
2023-08-08 22:25:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,63 +1,71 @@
 const registrationForm = document.getElementById('registrationForm');
-const dataTable = document.getElementById('dataTable').getElementsByTagName('tbody')[0];
+const dataBody = document.getElementById('dataBody');
 
-// Load saved data from web storage
-document.addEventListener('DOMContentLoaded', () => {
-  const savedData = JSON.parse(localStorage.getItem('registeredUsers')) || [];
-  savedData.forEach(userData => {
-    addRowToTable(userData);
-  });
-});
-
-registrationForm.addEventListener('submit', function (event) {
+registrationForm.addEventListener('submit', function(event) {
   event.preventDefault();
+  
+  const nameInput = document.getElementById('name');
+  const emailInput = document.getElementById('email');
+  const passwordInput = document.getElementById('password');
+  const dobInput = document.getElementById('dob');
+  const termsInput = document.getElementById('acceptTerms');
 
-  const name = document.getElementById('name').value;
-  const email = document.getElementById('email').value;
-  const password = document.getElementById('password').value;
-  const dob = document.getElementById('dob').value;
-  const acceptedTerms = document.getElementById('acceptedTerms').checked;
+  const name = nameInput.value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
+  const dob = dobInput.value;
+  const acceptTerms = termsInput.checked;
 
-  const age = calculateAge(dob);
-  if (age >= 18 && age <= 55 && name && email && password && acceptedTerms) {
-    const userData = { name, email, password, dob, acceptedTerms };
-    addRowToTable(userData);
-    saveToLocalStorage(userData);
-    registrationForm.reset();
-  } else {
-    alert('Please fill in all fields correctly and accept the terms. Your age should be between 18 and 55.');
+  const validEmail = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  
+  const today = new Date();
+  const dobDate = new Date(dob);
+  const ageInMilliseconds = today - dobDate;
+  const ageInYears = ageInMilliseconds / 1000 / 60 / 60 / 24 / 365.25;
+
+  if (ageInYears < 18 || ageInYears > 55 ) {
+    document.getElementById('birthday-error').innerHTML='<i class="fa-solid fa-circle-exclamation"></i>Your age must between 18 and 55 years to register.';
+    return;
   }
+
+  function store(){
+
+    const data = {
+      name: name,
+        email: email,
+        password: password,
+        dob: dob,
+        acceptTerms: acceptTerms,
+    };
+    var num=window.localStorage.length+1;
+    window.localStorage.setItem(num,JSON.stringify(data));  
+    
+      var myData = JSON.parse(window.localStorage.getItem(num));
+  const row = document.createElement('tr');
+
+    const nameCell = document.createElement('td');
+    nameCell.textContent = myData.name;
+    row.appendChild(nameCell);
+
+    const emailCell = document.createElement('td');
+    emailCell.textContent = myData.email;
+    row.appendChild(emailCell);
+
+    const passwordCell = document.createElement('td');
+    passwordCell.textContent = myData.password;
+    row.appendChild(passwordCell);
+
+    const dobCell = document.createElement('td');
+    dobCell.textContent = myData.dob;
+    row.appendChild(dobCell);
+
+    const acceptTermsCell = document.createElement('td');
+    acceptTermsCell.textContent = myData.acceptTerms;
+    row.appendChild(acceptTermsCell);
+
+    dataBody.appendChild(row);
+
+    
+}
+store();
 });
-
-function addRowToTable(userData) {
-  const newRow = dataTable.insertRow();
-  const cell1 = newRow.insertCell(0);
-  const cell2 = newRow.insertCell(1);
-  const cell3 = newRow.insertCell(2);
-  const cell4 = newRow.insertCell(3);
-  const cell5 = newRow.insertCell(4);
-
-  cell1.textContent = userData.name;
-  cell2.textContent = userData.email;
-  cell3.textContent = userData.password;
-  cell4.textContent = userData.dob;
-  cell5.textContent = userData.acceptedTerms ? 'Yes' : 'No';
-}
-
-function saveToLocalStorage(userData) {
-  const savedData = JSON.parse(localStorage.getItem('registeredUsers')) || [];
-  savedData.push(userData);
-  localStorage.setItem('registeredUsers', JSON.stringify(savedData));
-}
-
-function calculateAge(dob) {
-  const birthDate = new Date(dob);
-  const currentDate = new Date();
-  const age = currentDate.getFullYear() - birthDate.getFullYear();
-  const monthDiff = currentDate.getMonth() - birthDate.getMonth();
-  if (monthDiff < 0 || (monthDiff === 0 && currentDate.getDate() < birthDate.getDate())) {
-    return age - 1;
-  }
-  return age;
-}
-
